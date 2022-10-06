0758344edf3ea03050b619e482c5868a8ec362d6
LittleRookey <37283117+LittleRookey@users.noreply.github.com>
2022-10-06 09:33:22 -0400

    Added Hitscan
    - added hitscan type, setting bullet as visuals
    - projectile would not work yet

--- VolleyPaint/Assets/Bullet.cs
+++ VolleyPaint/Assets/Bullet.cs
@@ -20,13 +20,16 @@ public class Bullet : MonoBehaviour
     float ballShootForwardForce = 900f;
     float ballJumpUpForce = 900f;
 
+    bool isHitScan;
     GameObject hitImpact;
 
-    public void Setup(Vector3 shootDir, GameObject hitImpact)
+    public void Setup(Vector3 shootDir, float moveSpeed, GameObject hitImpact, bool isHitScan = true)
     {
-        
+        this.moveSpeed = moveSpeed;
         this.shootDir = shootDir;
         this.hitImpact = hitImpact;
+        this.isHitScan = isHitScan;
+
         Destroy(gameObject, 5f);
         if (hitImpact.GetComponent<AutoDestroy>() == null)
         {
@@ -34,12 +37,13 @@ public class Bullet : MonoBehaviour
         }
     }
 
-    public void Setup(Vector3 shootDir, float moveSpeed, eBulletType bulletType, GameObject hitImpact)
+    public void Setup(Vector3 shootDir, float moveSpeed, eBulletType bulletType, GameObject hitImpact, bool isHitScan = true)
     {
         this.shootDir = shootDir;
         this.moveSpeed = moveSpeed;
         this.hitImpact = hitImpact;
         this.bulletType = bulletType;
+        this.isHitScan = isHitScan;
         Destroy(gameObject, 5f);
         if (hitImpact.GetComponent<AutoDestroy>() == null)
         {
@@ -47,17 +51,6 @@ public class Bullet : MonoBehaviour
         }
     }
 
-    public void Setup(Vector3 shootDir, float moveSpeed, GameObject hitImpact)
-    {
-        this.shootDir = shootDir;
-        this.moveSpeed = moveSpeed;
-        this.hitImpact = hitImpact;
-        Destroy(gameObject, 5f);
-        if (hitImpact.GetComponent<AutoDestroy>() == null)
-        {
-            hitImpact.AddComponent<AutoDestroy>();
-        }
-    }
 
 
     // Update is called once per frame
@@ -74,25 +67,27 @@ public class Bullet : MonoBehaviour
             Debug.Log("Hit ball");
             //hit = true;
             Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
-            VolleyBall vb = collision.gameObject.GetComponent<VolleyBall>();
-
-            switch (bulletType)
+            GameObject vb = GameObject.FindGameObjectWithTag("Ball");
+            if (isHitScan)
             {
-                case eBulletType.Normal:
-                    rb.velocity = Vector3.zero;
-                    vb.transform.rotation = Camera.main.transform.rotation;
-                    rb.isKinematic = false;
-                    rb.velocity = Vector3.zero;
-                    rb.AddForce(vb.transform.forward * ballShootForwardForce);
-                    break;
-                case eBulletType.Jump:
-                    //vb.transform.rotation = Camera.main.transform.rotation;
-                    
-                    rb.isKinematic = false;
-                    //ball_rb.velocity = Vector3.zero;
-                    rb.AddForce(Vector3.up * ballJumpUpForce);
-                    break;
+                switch (bulletType)
+                {
+                    case eBulletType.Normal:
+                        rb.velocity = Vector3.zero;
+                        vb.transform.rotation = Camera.main.transform.rotation;
+                        rb.isKinematic = false;
+                        rb.velocity = Vector3.zero;
+                        rb.AddForce(vb.transform.forward * ballShootForwardForce);
+                        break;
+                    case eBulletType.Jump:
+                        //vb.transform.rotation = Camera.main.transform.rotation;
 
+                        rb.isKinematic = false;
+                        //ball_rb.velocity = Vector3.zero;
+                        rb.AddForce(Vector3.up * ballJumpUpForce);
+                        break;
+
+                }
             }
         }
 
