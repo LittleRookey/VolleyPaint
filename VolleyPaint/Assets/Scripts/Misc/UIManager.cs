b74dfb50fd9dff8e50aa578b406e7a59282724a1
Simon Levenstein <levenstein.s@northeastern.edu>
2022-11-11 00:02:10 -0500

    added hero select screen
    - added AssignAbilities script to assign abilities on button presses
    - added confirm button to appear after choosing an ability
    - moved all canvases under UIManger object
    - MainScene now uses newPlayer

--- VolleyPaint/Assets/Scripts/Misc/UIManager.cs
+++ VolleyPaint/Assets/Scripts/Misc/UIManager.cs
@@ -10,13 +10,19 @@ public class UIManager : MonoBehaviour
 
     [SerializeField] private Camera mainMenuCamera;
     [SerializeField] private GameObject flyCamera;
+    [SerializeField] private GameObject heroSelectCamera;
 
     [SerializeField] private GameObject scoreKeepingCanvas;
     [SerializeField] private GameObject mainMenuCanvas;
+    [SerializeField] private GameObject heroSelectCanvas;
+
+    [SerializeField] private GameObject confirmButton;
 
     private bool spectate;
-
     private bool gameStarted;
+    private bool heroSelected = false;
+
+    public GameObject userPlayer; // player that is being controlled by machine running this script
 
     // Start is called before the first frame update
     void Start()
@@ -34,9 +40,19 @@ public class UIManager : MonoBehaviour
         }
 
         // disable player in spectate mode
-        if (spectate && Camera.main != null)
+        // called in update so player is disabled as soon as network is connected (when there a main camera spawns)
+        if (!heroSelected && Camera.main && !userPlayer)
         {
-            Camera.main.transform.parent.gameObject.SetActive(!gameStarted);
+            userPlayer = Camera.main.transform.parent.gameObject;
+            userPlayer.SetActive(!gameStarted);
+            if (!spectate)
+            {
+                heroSelectCanvas.SetActive(true);
+                heroSelectCamera.SetActive(true);
+
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+            }
         }
     }
 
@@ -53,13 +69,28 @@ public class UIManager : MonoBehaviour
 
         if (spectate)
         {
-            flyCamera.SetActive(true);
+            flyCamera.SetActive(active);
         }
-
-       
     }
     public void SetSpectateActive(bool active)
     {
         spectate = active;
+    }
+
+    public void ReactivatePlayer()
+    {
+        heroSelectCanvas.SetActive(false);
+        heroSelectCamera.SetActive(false);
+
+        userPlayer.SetActive(true);
+        heroSelected = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void SetConfirmButtonActive()
+    {
+        confirmButton.SetActive(true);
     }
 }
