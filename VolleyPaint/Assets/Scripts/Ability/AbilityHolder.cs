74f436aff66eafe0c2b7a69c982b9fdce098c65b
LittleRookey <37283117+LittleRookey@users.noreply.github.com>
2022-11-16 00:21:27 -0500

    Added Ball ability work in network
    - made ball ability work on network
    - Added option to lock ability on abilityholder

--- VolleyPaint/Assets/Scripts/Ability/AbilityHolder.cs
+++ VolleyPaint/Assets/Scripts/Ability/AbilityHolder.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 using Unity.Netcode;
 
-public class AbilityHolder : NetworkBehaviour
+public class AbilityHolder : MonoBehaviour
 {
-    public bool isNetworkAbility; // whether the ability should be run on other clients(or servers) or not
-    public Ability ability;
-    float cooldownTime;
-    float activeTime;
-    bool isActive = false;
+    public Ability ability; // Ability Player will use
+    public bool isLocked = false; // locks the ability, but cooldown will run
+    [SerializeField]
+    private Image abilityIcon; // Ability Icon for player
+
+    float cooldownTime; // cooldown time of ability
+
+    float activeTime; // active time of ability, how long should it run the code and start cooldown?
+
+    bool isActive = false; // is ability active?
+
     enum AbilityState
     {
         ready, // when ability gets ready, no cooldown
@@ -39,21 +46,29 @@ public class AbilityHolder : NetworkBehaviour
 
     void Update()
     {
-        if (!IsOwner) return;
         // takes care of cooldown of the ability
         switch (state)
         {
             case AbilityState.ready:
+                if (isLocked) return;
                 if (Input.GetKeyDown(ability.key) && !isActive)
                 {
-                    StartSkill();
+                    OnAbilityStart?.Invoke(gameObject);
+                    state = AbilityState.active;
+                    activeTime = ability.activeTime;
+                    isActive = true;
 
                 }
                 break;
             case AbilityState.active:
                 if (activeTime > 0)
                 {
-                    RunningSkill();
+                    activeTime -= Time.deltaTime;
+                    OnAbilityRunning?.Invoke(gameObject);
+                    if (Input.GetKeyUp(ability.key) && !ability.Instantaneous) // ends active time when key is unpressed and ability is not instantanous 
+                    {
+                        TurnSkillOff();
+                    }
                 }
                 else
                 {
@@ -64,97 +79,42 @@ public class AbilityHolder : NetworkBehaviour
                 if (cooldownTime > 0)
                 {
                     cooldownTime -= Time.deltaTime;
+                    if (abilityIcon != null)
+                        abilityIcon.fillAmount = 1f - cooldownTime / ability.coolDownTime;
                 }
                 else
                 {
+                    cooldownTime = 0f;
                     state = AbilityState.ready;
                 }
                 break;
         }
     }
 
-    private void StartSkill()
-    { 
-        OnAbilityStart?.Invoke(gameObject);
-        state = AbilityState.active;
-        activeTime = ability.activeTime;
-        isActive = true;
-        if (isNetworkAbility)
+    /// <summary>
+    /// Returns the Ability holder of the given ability type 
+    /// </summary>
+    /// <param name="target">the target GameObject AbilityHolder is attached to</param>
+    /// <param name="abilityType">the type of ability to be returned</param>
+    /// <returns> Returns the Ability holder that carries the given ability type </returns>
+    public static AbilityHolder GetAbilityHolderOfType(GameObject target, eAbilityType abilityType)
+    {
+        AbilityHolder[] abilityHolders = target.GetComponents<AbilityHolder>();
+        foreach (AbilityHolder ab in abilityHolders)
         {
-            StartSkillServerRPC();
+            if (ab.ability.abilityType == abilityType)
+            {
+                return ab;
+            }
         }
+        return null;
     }
 
-    [ServerRpc]
-    private void StartSkillServerRPC()
-    {
-        StartSkillClientRPC();
-    }
-
-    [ClientRpc]
-    private void StartSkillClientRPC()
-    {
-        if (IsOwner) return;
-        OnAbilityStart?.Invoke(gameObject);
-        state = AbilityState.active;
-        activeTime = ability.activeTime;
-        isActive = true;
-    }
-
-    private void RunningSkill()
-    {
-        activeTime -= Time.deltaTime;
-
-        OnAbilityRunning?.Invoke(gameObject);
-        if (Input.GetKeyUp(ability.key) && !ability.Instantaneous) // ends active time when key is unpressed and ability is not instantanous 
-        {
-            TurnSkillOff();
-        }
-    }
-
-    [ServerRpc]
-    private void RunningSkillServerRPC()
-    {
-        RunningSkillClientRPC();
-    }
-
-
-    [ClientRpc]
-    private void RunningSkillClientRPC()
-    {
-        if (IsOwner) return;
-        activeTime -= Time.deltaTime;
-
-        OnAbilityRunning?.Invoke(gameObject);
-        if (Input.GetKeyUp(ability.key) && !ability.Instantaneous) // ends active time when key is unpressed and ability is not instantanous 
-        {
-            TurnSkillOff();
-        }
-    }
-
-    // Disactivate the skill and run cooldown and set active time to 0
+    /// <summary>
+    /// Disactivate the skill and run cooldown and set active time to 0
+    /// </summary>
     private void TurnSkillOff()
     {
-        activeTime = 0f;
-        isActive = false;
-        state = AbilityState.cooldown;
-        cooldownTime = ability.coolDownTime;
-        OnAbilityEnd?.Invoke(gameObject);
-        if (isNetworkAbility)
-        {
-            TurnSkillOffServerRPC();
-        }
-    }
-
-    [ServerRpc]
-    private void TurnSkillOffServerRPC()
-    {
-        TurnSkillOffClientRPC();
-    }
-    [ClientRpc]
-    private void TurnSkillOffClientRPC()
-    {
-        if (IsOwner) return;
         activeTime = 0f;
         isActive = false;
         state = AbilityState.cooldown;
--- VolleyPaint/Assets/Scripts/BallBehaviour.cs
+++ VolleyPaint/Assets/Scripts/BallBehaviour.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using Blobcreate.Universal;
 using Blobcreate.ProjectileToolkit;
+using Unity.Netcode;
+
 
 public class BallBehaviour : ProjectileBehaviour
 {
@@ -12,8 +15,21 @@ public class BallBehaviour : ProjectileBehaviour
     public LayerMask scanMask;
     public LayerMask selfMask;
     //public bool isCurveBall;
+    public LayerMask groundMask;
+    public float torqueForce = 0f;
+    public float smallA = -0.1f;
+    public float bigA = -0.01f;
+    public float lerpSpeed = 5f;
+    //private TrajectoryPredictor trajectory;
+    //public bool drawLine;
+    //public bool faceDirection;
+    float currentA;
+    float currentTorque;
 
     Collider[] result = new Collider[16];
+
+    public UnityAction OnBallEnterTeam1;
+    public UnityAction OnBallEnterTeam2;
 
     protected override void OnLaunch()
     {
@@ -35,6 +51,36 @@ public class BallBehaviour : ProjectileBehaviour
         }
 
         base.Explosion(collision);
+    }
+
+    private void Fire(Vector3 target, float tCurrentA)
+    {
+        Launch(target);
+        rb.isKinematic = false;
+        // Magic happens!
+        var f = Projectile.VelocityByA(transform.position, target, tCurrentA);
+        Debug.Log($"ball pos: {transform.position}  targetPos: {target}");
+        rb.AddForce(f, ForceMode.VelocityChange);
+
+        // Add some torque, not necessary, but interesting.
+        var t = Vector3.Lerp(torqueForce * Random.onUnitSphere,
+            torqueForce * (target - transform.position).normalized, currentTorque);
+        rb.AddTorque(t, ForceMode.VelocityChange);
+    }
+    [ServerRpc]
+    public void FireServerRPC(Vector3 target, float tCurrentA)
+    {
+        Fire(target, tCurrentA);
+        //var b = Instantiate(bulletPrefab, launchPoint.position, launchPoint.rotation);
+        FireClientRPC(target, tCurrentA);
+
+    }
+
+    [ClientRpc]
+    private void FireClientRPC(Vector3 target, float tCurrentA)
+    {
+        if (IsOwner) return;
+        Fire(target, tCurrentA);
     }
 
     private void OnTriggerEnter(Collider other)
